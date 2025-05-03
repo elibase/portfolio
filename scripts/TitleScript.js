e5b1944a9a5e639b3adc30224ab13ab6062f7995
elibase <eli@base>
2025-05-03 00:26:34 -0400

    navigation content added (switching to laptop)

---
+++ scripts/TitleScript.js
@@ -0,0 +1,11 @@
+window.addEventListener( 
+    "blur", () =>  {
+        document.title = "E L I B A S E K E";
+    }
+)
+
+window.addEventListener( 
+    "focus", () =>  {
+        document.title = "Eli Baseke";
+    }
+)
